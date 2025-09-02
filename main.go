9f084698f15cf9d87428a71223ffe6752a2ac51c
Kaleab Yemane <kaleaby49@gmail.com>
2025-09-02 15:12:18 +0300

    Add more Request types(Post...)

--- main.go
+++ main.go
@@ -6,31 +6,57 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
-	// CLI flag
+	
 	url := flag.String("url", "", "URL to fetch")
+	requestType := flag.String("request", "GET", "HTTP method: GET, POST, etc.") // new flag
+	data := flag.String("data", "", "Data to send with POST")
+	verbose := flag.Bool("verbose", false, "Show verbose output")
 
 	flag.Parse()
 
 	if *url == "" {
-		fmt.Println("Usage: go run main.go -url https://example.com")
+		fmt.Println("Usage: go run main.go -url https://example.com [-request GET/POST] [-data 'body']")
 		return
 	}
 
-	// send GET request
-	resp, err := http.Get(*url)
+	// Prepare the request
+	var req *http.Request
+	var err error
+
+	if strings.ToUpper(*requestType) == "POST" {
+		req, err = http.NewRequest("POST", *url, strings.NewReader(*data)) // handle POST body
+	} else {
+		req, err = http.NewRequest(strings.ToUpper(*requestType), *url, nil)
+	}
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Creating request failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	if *verbose {
+		fmt.Println("Request Method:", req.Method)
+		fmt.Println("Request URL:", req.URL.String())
+		if *data != "" {
+			fmt.Println("Request Body:", *data)
+		}
+	}
+
+	// Send the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Request failed: %v\n", err)
 		os.Exit(1)
 	}
-	defer resp.Body.Close() // close when done
+	defer resp.Body.Close()
 
-	// print status
+	// Print response
 	fmt.Println("Status:", resp.Status)
-
-	// print body
 	_, err = io.Copy(os.Stdout, resp.Body)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Reading body failed: %v\n", err)
